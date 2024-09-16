39237b36e17891f82ae7120c22440eff75260b8c
absra47 <absragirma630@gmail.com>
2024-09-16 14:57:51 +0300

    Authentication using jwt and bcryptjs password encry

--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -1,10 +1,42 @@
+import { useState } from "react";
+
 export default function RegisterPage() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function register(ev) {
+    ev.preventDefault();
+
+    const response = await fetch("http://localhost:4000/register", {
+      method: "POST",
+      body: JSON.stringify({ username, password }),
+      headers: { "content-Type": "application/json" },
+    });
+    if (response.status === 200) {
+      alert("registration successful");
+    } else {
+      alert("registration failed");
+    }
+  }
   return (
-    <form className="register">
+    <form className="register" onSubmit={register}>
       <h1>Register</h1>
-      <input type="text" placeholder="Username" />
-      <input type="password" placeholder="Password" />
+      <input
+        type="text"
+        placeholder="Username"
+        value={username}
+        onChange={(ev) => setUsername(ev.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(ev) => setPassword(ev.target.value)}
+      />
       <button>Register</button>
     </form>
   );
 }
+
+//
+//;
